5e22f62ee07b2a4726f06ccf54dc5c419f09f36e
jennyPI <hinje17@gmail.com>
2021-09-06 23:22:08 -0600

    results

--- test_file.py
+++ test_file.py
@@ -5,6 +5,11 @@ GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
     buttonState = GPIO.input(16)
-    if buttonsState == True:
+    if buttonState == True:
         print("pressed")
+    if buttonState == False:
+        print ("not pressed")
+        
+        
+GPIO.cleanup()
         
