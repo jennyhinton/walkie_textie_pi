ffec21152c6851b52d3ee1b98b96a0d79530c540
jennyPI <hinje17@gmail.com>
2021-09-06 23:42:48 -0600

    test updates

--- test_file.py
+++ test_file.py
@@ -1,13 +1,15 @@
 import RPi.GPIO as GPIO
 
-def power_callback(self, channel):
+GPIO.cleanup()
+
+def power_callback(channel):
         print ("power pushed")  #button actions
         
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(16, GPIO.FALLING, callback=self.power_callback)
+
+while (True):
+    GPIO.add_event_detect(16, GPIO.RISING, callback=power_callback)
 
         
-        
-GPIO.cleanup()
-        
+                
