69ec3e408f109fdd85512aa60b3fab09ee65d219
Jenny <hinje17@gmail.com>
2021-09-06 23:25:44 -0600

    adding new test

--- test_file.py
+++ test_file.py
@@ -1,14 +1,12 @@
 import RPi.GPIO as GPIO
 
+def power_callback(self, channel):
+        print ("power pushed")  #button actions
+        
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(16, GPIO.FALLING, callback=self.power_callback)
 
-while True:
-    buttonState = GPIO.input(16)
-    if buttonState == True:
-        print("pressed")
-    if buttonState == False:
-        print ("not pressed")
         
         
 GPIO.cleanup()
