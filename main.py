99c4ad1a06042c4f2e8e23b94ee6b992a4cf0117
Jenny <hinje17@gmail.com>
2021-09-24 19:36:50 -0600

    checking test

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from buttons import Buttons
 from keyboard import Keyboard
 
 def main():
-    keyboard = Keyboard()
-    #buttons = Buttons()
+    #keyboard = Keyboard()
+    buttons = Buttons()
     #screen = Screen()
     print("back in main")
     
--- test_file.py
+++ test_file.py
@@ -6,8 +6,8 @@ def power_callback(channel):
         print ("power pushed")  #button actions
         
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(16, GPIO.FALLING, callback=power_callback, bouncetime=500)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_down)
+GPIO.add_event_detect(16, GPIO.RISING, callback=power_callback, bouncetime=500)
 
 while (True):
     pass
