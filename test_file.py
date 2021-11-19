babf9492f3af3931b87a849bd5eed71ff9c73f1a
Jenny <hinje17@gmail.com>
2021-11-19 01:17:15 -0700

    to pi

--- test_file.py
+++ test_file.py
@@ -5,8 +5,8 @@ def power_callback(channel):
         print ("power pushed")  #button actions
         
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.add_event_detect(14, GPIO.RISING, callback=power_callback, bouncetime=500)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(8, GPIO.RISING, callback=power_callback, bouncetime=500)
 
 raw_input("Hit enter to end")
 
