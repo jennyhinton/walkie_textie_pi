4fb7ffeecce6b7238849801608b86fbb5c52a9d5
Jenny <hinje17@gmail.com>
2021-11-21 22:09:38 -0700

    to pi

--- test_file.py
+++ test_file.py
@@ -6,7 +6,7 @@ def power_callback(channel):
         
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(8, GPIO.RISING, callback=power_callback)#, bouncetime=500)
+GPIO.add_event_detect(8, GPIO.RISING, callback=power_callback, bouncetime=500)
 
 raw_input("Hit enter to end")
 
