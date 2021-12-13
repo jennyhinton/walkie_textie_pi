534719a8aa319ac49e4784347b888da235b36f92
Jenny <hinje17@gmail.com>
2021-12-13 00:26:40 -0700

    to pi

--- main.py
+++ main.py
@@ -1,21 +1,28 @@
 from screen import Screen
 from buttons import Buttons
-from keyboard import Keyboard
+#from keyboard import Keyboard
 from letter import Alphabet
+from pynput import keyboard
+
+screen = Screen()
+
+#pip install pynput
+def on_press (key):
+    try:
+        screen.insert_character(Alphabet[key.char])
+    except AttributeError:
+        print("Key not defined yet/ keyboard error")
+
+with keyboard.Listener(
+        on_press= on_press) as listener:
+    listener.start()
 
 
 def main():
     #keyboard = Keyboard()
     #buttons = Buttons()
-    screen = Screen()
     screen.all_pixels_off()
-    
-    print("letters")
 
-    for _ in range(50):
-        screen.insert_character(Alphabet['A'])
-        screen.insert_character(Alphabet['B'])
-        screen.insert_character(Alphabet['C'])
     
 if __name__ == "__main__":
     main()
