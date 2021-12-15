c08848e00135449b3b9d7948d18f2d28fd799930
Jenny <hinje17@gmail.com>
2021-12-14 23:39:33 -0700

    to pi

--- buttons.py
+++ buttons.py
@@ -285,7 +285,9 @@ class Buttons(object):
     def button_pressed(self, value):
         self._button_pressed = value
         callback = self.callbacks[0]
+        print("Length of callbacks: " + str(len(self.callbacks)))
         if callback:
+            print("Type of callback: " + type(callback))
             callback()
 
     def bind(self, callback):
